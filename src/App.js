baaa57b3702f401e86f44a2c8ea36270978ac5c0
Phil Ko <35305115+KeonwooKo@users.noreply.github.com>
2022-07-12 20:03:39 +0900

    USD -> CoinValue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [Loading, setLoading] = useState(true);
+  const [Coins, setCoins] = useState([]);
+  const [Amount, setAmount] = useState(0);
+  const [CoinPrice, setCoinPrice] = useState();
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const changeSelect = (event) => {
+    setCoinPrice(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins ! {Loading ? "" : `(${Coins.length})`}</h1>
+      {Loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={changeSelect}>
+          <option>Select Coin !</option>
+          {Coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <form>
+        <label htmlFor="usd">USD : </label>
+        <input
+          id="usd"
+          type="number"
+          value={Amount}
+          onChange={onChange}
+          placeholder="USD you have?"
+        ></input>
+        <h3>You have {Amount / CoinPrice} </h3>
+      </form>
+      <div> </div>
     </div>
   );
 }
